74e735108be16c1917ee13b051925f10950589a2
sidarth mishra <smishra9120@gmail.com>
2022-05-15 18:00:32 +0530

    update collection name

--- src/Pages/Collection/Collection.jsx
+++ src/Pages/Collection/Collection.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { homeStartLoading } from "../../actions/game";
@@ -14,20 +14,28 @@ import {useParams} from "react-router-dom";
 
 
 export const Collection = () => {
+  const [collectionName, setCollectionName] = useState(null);
   const { t } = useTranslation(["home"]);
   const dispatch = useDispatch();
   const { id } = useParams();
   const { info} = useSelector((state) => state.collection);
   const { wallet, network} = useSelector((state) => state.wallet);
 
-  const { last_release, featured, loaded } = useSelector((state) => state.games);
+  const { last_release, featured } = useSelector((state) => state.games);
   useEffect(() => {
     dispatch(collectionInfoLoading(parseInt(id || 1)));
   }, [dispatch,wallet,network, id]);
+  useEffect(() => {
+    dispatch(homeStartLoading());
+  }, [dispatch]);
+  useEffect(()=>{
+    let name = featured[parseInt(id) -1]?.name
+    setCollectionName(name)
+  },[featured,id])
 
   return (
     <>
-      <CollectionHeader featured={null} loaded={false} info={info} />
+      <CollectionHeader featured={null} loaded={false} info={info} collectionName={collectionName}  />
       <Box mt={{ base: "20%", md: "8%" }}>
         <RegularGrid collectionId={id || 1} lastReleases={last_release} loaded={false}  title="NFTs" info={info} CardComponent={<Card />} />
       </Box>
